f7a8d8bbe04971b5d68021669d5bef0c87b75d69
王习习 <1521185796@qq.com>
2019-09-02 19:22:31 +0800

    接口访问-axios拦截器-统一处理请求token

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './permission'
 import ElementUI from 'element-ui' // 框架组件
 import Component from './components' // 自定义组件
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 import './assets/index.less' // 引入初始化样式
-import axios from 'axios'
+import axios from './utils/axios.config'
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
---
+++ src/utils/axios.config.js
@@ -0,0 +1,11 @@
+// 处理axios 拦截器
+import axios from 'axios'
+// 请求拦截器
+axios.interceptors.request.use(function (config) {
+  // config 是axios 的默认请求配置 和传入的配置 结合出来的
+  // 必须将config return 不return 不行
+  let token = window.localStorage.getItem('user-token')
+  config.headers['Authorization'] = `Bearer ${token}`
+  return config
+}, function () { })
+export default axios
