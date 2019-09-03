4dc09f5c9506a5f5ecd4d9961073d94ca747586d
王习习 <1521185796@qq.com>
2019-09-03 19:27:18 +0800

    面包屑组件封装

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import LayoutAside from './home/layout-aside.vue'
 import LaoutHeader from './home/layout-header.vue'
+import BreadCrumb from './common/bread-crumb.vue'
 // 所有自定义插件
 export default {
   install (Vue) {
     Vue.component('layout-aside', LayoutAside) // 左侧导航事件
     Vue.component('layout-header', LaoutHeader)// 注册头部事件
+    Vue.component('bread-crumb', BreadCrumb)// 注册面包屑组件
   }
 }
--- src/utils/axios.config.js
+++ src/utils/axios.config.js
@@ -1,5 +1,6 @@
 // 处理axios 拦截器
 import axios from 'axios'
+import router from '../router'
 // 请求拦截器
 axios.interceptors.request.use(function (config) {
   // config 是axios 的默认请求配置 和传入的配置 结合出来的
@@ -11,5 +12,32 @@ axios.interceptors.request.use(function (config) {
 // 响应拦截器
 axios.interceptors.response.use(function (response) {
   return response.data ? response.data : {}
-}, function () {})
+}, function (error) {
+  let status = error.response.status
+  let message = ' '
+  switch (status) {
+    case 400:
+      message = '请求参数错误'
+      break
+    case 403:
+      message = '用户非实名认证用户，无权限登录'
+      break
+    case 507:
+      message = '服务器或数据库异常'
+      break
+    case 404:
+      message = '手机号不正确'
+      break
+    case 401:
+      window.localStorage.clear()
+      router.push('/login')
+      break
+    default:
+      message = '未知错误'
+      break
+  }
+  // eslint-disable-next-line no-undef
+  Message({ type: 'warning', message })
+  return new Promise(function () {})
+})
 export default axios
